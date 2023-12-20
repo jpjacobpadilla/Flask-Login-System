a47ef405b6af6fe4ccbabf3c0b697fbd4a8a5e0f
Jacob Padilla <jp@jacobpadilla.com>
2023-12-20 15:58:25 -0500

    set session

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import (
 )
 
 from create_database import setup_database
-from login_required import login_required
+from utils import login_required, set_session
 
 
 app = Flask(__name__)
@@ -22,98 +22,7 @@ setup_database(name=database)
 
 @app.route('/login')
 def login():
-    print(session)
     return render_template('login.html')
-
-
-@app.route('/verify-user', methods=['POST'])
-def verify_user():
-    username = request.form['username']
-    password = request.form['password']
-    
-    query = 'select id, username, password, email from users where username = :username'
-
-    with contextlib.closing(sqlite3.connect(database)) as conn:
-        with conn:
-            account = conn.execute(query, {'username': username}).fetchone()
-
-    if not account: return render_template('login.html', error_msg='Username does not exist')
-
-    try:
-        ph = PasswordHasher()
-        ph.verify(account[2], password)
-
-    except VerifyMismatchError:
-        return render_template('login.html', error_msg='Incorrect password')
-
-    hashed_password = ph.hash(password)
-    if ph.check_needs_rehash(hashed_password):
-        query = 'update set password = :password where id = :id'
-        params = {'password': hashed_password, 'id': account[0]}
-
-        with contextlib.closing(sqlite3.connect(database)) as conn:
-            with conn:
-                conn.execute(query, params)
-
-    session['logged_in'] = True
-    session['id'] = account[0]
-    session['username'] = account[1]
-    session['email'] = account[3]
-
-    if 'remember-me' in request.form:
-        session.permanent = True 
-    
-    return redirect('/')
-
-
-@app.route('/register-user', methods=['POST'])
-def register_user():
-    password = request.form['password']
-    confirm_password = request.form['confirm-password']
-    username = request.form['username']
-    email = request.form['email']
-
-    if password != confirm_password:
-        return render_template('register.html', error_msg='Passwords do not match')
-    if not re.match(r'^[a-zA-Z0-9]+$', username):
-        return render_template('register.html', error_msg='Username must only be letters and numbers')
-    if len(username) < 3:
-        return render_template('register.html', error_msg='Username must be 3 or more characters')
-    if len(username) > 25:
-        return render_template('register.html', error_msg='Username must be 25 or less characters')
-
-    query = 'select id from users where username = :username;'
-    with contextlib.closing(sqlite3.connect(database)) as conn:
-        with conn:
-            result = conn.execute(query, {'username': username}).fetchone()
-    if result:
-        return render_template('register.html', error_msg='Username already exists')
-
-
-    pw = PasswordHasher()
-    hashed_password = pw.hash(password)
-
-    query = 'insert into users(username, password, email) values (:username, :password, :email);'
-
-    params = {
-        'username': username,
-        'password': hashed_password,
-        'email': email
-    }
-
-    with contextlib.closing(sqlite3.connect(database)) as conn:
-        with conn:
-            result = conn.execute(query, params)
-
-    session['logged_in'] = True
-    session['id'] = result.lastrowid
-    session['username'] = username
-    session['email'] = email
-
-    if 'remember-me' in request.form:
-        session.permanent = True 
-
-    return redirect('/')
 
 
 @app.route('/register')
@@ -133,6 +42,100 @@ def logout():
 def index():
     print(f'User data: {session}')
     return render_template('index.html', username=session.get('username'))
+
+
+@app.route('/verify-user', methods=['POST'])
+def verify_user():
+    # Set data to variables
+    username = request.form['username']
+    password = request.form['password']
+    
+    # Attempt to query associated user data
+    query = 'select id, username, password, email from users where username = :username'
+
+    with contextlib.closing(sqlite3.connect(database)) as conn:
+        with conn:
+            account = conn.execute(query, {'username': username}).fetchone()
+
+    if not account: 
+        return render_template('login.html', error_msg='Username does not exist')
+
+    # Verify password
+    try:
+        ph = PasswordHasher()
+        ph.verify(account[2], password)
+    except VerifyMismatchError:
+        return render_template('login.html', error_msg='Incorrect password')
+
+    # Check if password hash needs to be updated
+    hashed_password = ph.hash(password)
+    if ph.check_needs_rehash(hashed_password):
+        query = 'update set password = :password where id = :id'
+        params = {'password': hashed_password, 'id': account[0]}
+
+        with contextlib.closing(sqlite3.connect(database)) as conn:
+            with conn:
+                conn.execute(query, params)
+
+    # Set cookie for user session
+    set_session(
+        id=account[0], 
+        username=account[1], 
+        email=account[3], 
+        remember_me='remember-me' in request.form
+    )
+    
+    return redirect('/')
+
+
+@app.route('/register-user', methods=['POST'])
+def register_user():
+    # Store data to variables 
+    password = request.form['password']
+    confirm_password = request.form['confirm-password']
+    username = request.form['username']
+    email = request.form['email']
+
+    # Verify data
+    if password != confirm_password:
+        return render_template('register.html', error_msg='Passwords do not match')
+    if not re.match(r'^[a-zA-Z0-9]+$', username):
+        return render_template('register.html', error_msg='Username must only be letters and numbers')
+    if len(username) < 3:
+        return render_template('register.html', error_msg='Username must be 3 or more characters')
+    if len(username) > 25:
+        return render_template('register.html', error_msg='Username must be 25 or less characters')
+
+    query = 'select id from users where username = :username;'
+    with contextlib.closing(sqlite3.connect(database)) as conn:
+        with conn:
+            result = conn.execute(query, {'username': username}).fetchone()
+    if result:
+        return render_template('register.html', error_msg='Username already exists')
+
+    # Create password hash
+    pw = PasswordHasher()
+    hashed_password = pw.hash(password)
+
+    query = 'insert into users(username, password, email) values (:username, :password, :email);'
+    params = {
+        'username': username,
+        'password': hashed_password,
+        'email': email
+    }
+
+    with contextlib.closing(sqlite3.connect(database)) as conn:
+        with conn:
+            result = conn.execute(query, params)
+
+    # We can log in the user right away since no email verification
+    set_session(
+        id=result.lastrowid, 
+        username=username, 
+        email=email
+    )
+
+    return redirect('/')
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,13 +1,24 @@
 from flask import redirect, url_for, session
 from functools import wraps
 
+
 def login_required(func):
     @wraps(func)
     def decorator(*args, **kwargs):
         # Check if user is logged in
-        if 'logged_in' not in session:
+        if 'id' not in session:
             return redirect(url_for('login')) # User is not logged in; redirect to login page
             
         return func(*args, **kwargs)
     
     return decorator
+
+
+def set_session(id: int, username: str, email: str,
+    remember_me: bool = False) -> None:
+
+    session['id'] = id
+    session['username'] = username
+    session['email'] = email
+
+    session.permanent = remember_me
